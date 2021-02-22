88cffdf874aef3b49b4d4b8b9dfed92027bceefb
saranonearth <saran.suresh@outlook.com>
2021-02-22 17:46:18 +0530

    Added log types | closes #1

---
+++ example/dev.ts
@@ -0,0 +1,10 @@
+import { Ayioo } from '../src/';
+
+(() => {
+  Ayioo.configure(
+    `ODA3MzUzNDEzNjQ5MzAxNTA0.YB2wdQ.GaKfFTyBT69R7O-46C6-SvqvDGI`,
+    '807358792621752424',
+  );
+  Ayioo.log(new Date().toISOString());
+  Ayioo.warn('Hey');
+})();
